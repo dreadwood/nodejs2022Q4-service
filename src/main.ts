ca8073fa4fce9ad814db2018d5372f977e278b8d
srg <skabassjob@gmail.com>
2023-02-06 13:29:58 +0500

    feat: set read port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { DEFAULT_PORT } from './const';
+
+dotenv.config();
+const port = process.env.PORT ?? DEFAULT_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
+
+  console.log(`Listen port: ${port}`); // TODO: 2023-02-06 / remove
 }
+
 bootstrap();
