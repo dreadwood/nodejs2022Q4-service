9baa70bcbfa7d4b5f3ab16f22c8243d6ad854cc7
srg <skabassjob@gmail.com>
2023-02-07 01:18:51 +0500

    feat: add validation for users

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { DEFAULT_PORT } from './const';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 const port = process.env.PORT ?? DEFAULT_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 
   console.log(`Listen port: ${port}`); // TODO: 2023-02-06 / remove
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,15 @@
+import { IsString, IsUUID } from 'class-validator';
+
 export class UserEntity {
+  @IsUUID(4)
   id: string;
+
+  @IsString()
   login: string;
+
+  @IsString()
   password: string;
+
   version: number;
   createdAt: number;
   updatedAt: number;
